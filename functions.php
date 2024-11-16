23ee6a7daa816b3a839dd8c37794e6839bafce29
Mizanur Rahman <nickty.86@gmail.com>
2024-11-16 13:00:22 +0300

    func p

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 if (!defined('ABSPATH')) exit; // Exit if accessed directly
 
+// Theme Setup
 function gulfdevs_setup() {
     load_theme_textdomain('gulfdevs', get_template_directory() . '/languages');
     add_theme_support('title-tag');
@@ -11,20 +12,27 @@ function gulfdevs_setup() {
 }
 add_action('after_setup_theme', 'gulfdevs_setup');
 
-function gulfdevs_enqueue_scripts() {
+// Enqueue Scripts and Styles
+function gulfdevs_enqueue_assets() {
+    // Enqueue main theme stylesheet
     wp_enqueue_style('gulfdevs-style', get_stylesheet_uri(), array(), '1.0.1');
+
+    // Enqueue Tailwind CSS
     wp_enqueue_style('gulfdevs-tailwind', get_template_directory_uri() . '/assets/css/tailwind.css', array(), '1.0.1');
+
+    // Enqueue main CSS with cache busting
+    $css_file = get_stylesheet_directory() . '/assets/css/style.css';
+    if (file_exists($css_file)) {
+        $css_version = filemtime($css_file);
+        wp_enqueue_style('gulfdevs-new-style', get_stylesheet_directory_uri() . '/assets/css/style.css', array(), $css_version);
+    }
+
+    // Enqueue main JavaScript file
     wp_enqueue_script('gulfdevs-script', get_template_directory_uri() . '/assets/js/script.js', array(), '1.0.0', true);
 }
-add_action('wp_enqueue_scripts', 'gulfdevs_enqueue_scripts');
+add_action('wp_enqueue_scripts', 'gulfdevs_enqueue_assets');
 
-function gulfdevs_enqueue_styles() {
-    $css_version = filemtime(get_stylesheet_directory() . '/assets/css/style.css');
-    wp_enqueue_style('gulfdevs-new-style', get_stylesheet_directory_uri() . '/assets/css/style.css', array(), $css_version);
-}
-add_action('wp_enqueue_scripts', 'gulfdevs_enqueue_styles');
-
-
+// Customizer Settings
 function gulfdevs_customize_register($wp_customize) {
     $wp_customize->add_section('gulfdevs_hero_section', array(
         'title' => __('Hero Section', 'gulfdevs'),
