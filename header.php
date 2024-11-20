069301667382057948c5c8ed5121849fc0a0e687
Mizanur Rahman <nickty.86@gmail.com>
2024-11-20 20:40:05 +0300

    favicon

--- header.php
+++ header.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png" type="image/png">
+
     <?php wp_head(); ?>
 </head>
 
